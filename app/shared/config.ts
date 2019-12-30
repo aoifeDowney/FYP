a1dbb271786b07bcbd8ae534a08f4bf61f850c38
aoifeDowney <aoifestard@gmail.com>
2019-12-30 19:17:27 +0000

    first commit

---
+++ app/shared/config.ts
@@ -0,0 +1,6 @@
+export class Config {
+  static apiUrl = "https://baas.kinvey.com/";
+  static appKey = "kid_BJAu9c4iS";
+  static authHeader = "Basic a2lkX0JKQXU5YzRpUzoyNjUzNWZjNTIwNTY0ZTM2YTZjOGU0MjI3ZTM3Y2ExOA==";
+  static token = "";
+}
