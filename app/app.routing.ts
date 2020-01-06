de0fb78c13fc1689f73bf6893a9eaf9f8c8bccd4
aoifeDowney <aoifestard@gmail.com>
2020-01-06 15:06:12 +0000

    changed list to IndividualProfile

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -1,15 +1,12 @@
 import { LoginComponent } from "./login/login.component";
-import { ListComponent } from "./list/list.component";
-import { IndividualHomeComponent } from "./IndividualProfile/individualHome.component";
+import { IndividualProfileComponent } from "./IndividualProfile/IndividualProfile.component";
 
 export const routes = [
     { path: "", component: LoginComponent },
-    { path: "list", component: ListComponent },
-    { path: "individualHome", component: IndividualHomeComponent }
+    { path: "IndividualProfile", component: IndividualProfileComponent }
 ];
 
 export const navigatableComponents = [
     LoginComponent,
-    ListComponent,
-    IndividualHomeComponent
+    IndividualProfileComponent
 ];
