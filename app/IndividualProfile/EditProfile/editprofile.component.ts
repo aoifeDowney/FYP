712fd3fe663ce7da67e53e7647e9772d57dcd765
aoifeDowney <aoifestard@gmail.com>
2020-01-16 17:17:09 +0000

    created service to connect profile to the backend

--- app/IndividualProfile/EditProfile/editprofile.component.ts
+++ app/IndividualProfile/EditProfile/editprofile.component.ts
@@ -4,24 +4,38 @@ import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 import * as dialogs from "tns-core-modules/ui/dialogs";
 import * as Kinvey from "kinvey-nativescript-sdk";
 
+import { UserProfile } from "../../shared/userprofile/userprofile.model";
+import { UserProfileSerive } from "../../shared/userprofile/userprofile.service";
+
 
 @Component({
   selector: "gr-profile",
   templateUrl: "./editprofile.component.html",
-  styleUrls: ["./editprofile.component.css"]
+  styleUrls: ["./editprofile.component.css"],
+  providers: [UserProfileSerive]
 })
 
 export class EditProfileComponent implements OnInit {
 
+  profile: Array<UserProfile> = [];
+  isLoading = false;
+  listLoaded = false;
+
   //username: any = Kinvey.User.getActiveUser();
   username: string = "Aoife";
 
-  constructor() {
-    // Use the component constructor to inject providers.
-  }
+  constructor(private userProfileSerive: UserProfileSerive) {}
 
   ngOnInit(): void {
-    // Init your component properties here.
+    this.isLoading = true;
+        this.userProfileSerive.load()
+            .subscribe(loadedGroceries => {
+                loadedGroceries.forEach((groceryObject) => {
+                    this.profile.unshift(groceryObject);
+                });
+                this.isLoading = false;
+                this.listLoaded = true;
+            });
   }
 
   onDrawerButtonTap(): void {
---
+++ app/shared/userprofile/userprofile.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders, HttpResponse } from "@angular/common/http";
+import { Observable, throwError } from "rxjs";
+import { catchError, map, tap } from "rxjs/operators";
+
+import { Config } from "../config";
+import { UserProfile } from "./userprofile.model";
+
+@Injectable()
+export class UserProfileSerive {
+
+    baseUrl = Config.apiUrl + "appdata/" + Config.appKey + "/UserProfile";
+
+    constructor(private http: HttpClient) { }
+
+    load() {
+        // Kinvey-specific syntax to sort the groceries by last modified time. Don’t worry about the details here.
+        let params = {
+            "sort": "{\"_kmd.lmt\": 1}"
+        };
+
+        return this.http.get(this.baseUrl, {
+            headers: this.getCommonHeaders(),
+            params: params
+        }).pipe(
+            map((data: []) => {
+                let profile = [];
+                data.forEach((userProfile) => {
+                    profile.push(new UserProfile((<any>userProfile)._id, (<any>userProfile).Username));
+                });
+                return profile;
+            }),
+            catchError(this.handleErrors)
+        );
+    }
+
+    getCommonHeaders() {
+        return {
+            "Content-Type": "application/json",
+            "Authorization": "Kinvey " + Config.token
+        }
+    }
+
+    handleErrors(error: Response) {
+        console.log(JSON.stringify(error.json()));
+        return throwError(error);
+    }
+
+}
