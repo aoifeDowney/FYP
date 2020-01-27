f9da0c48d0fca0c826905b418ed25f4a1fdacbed
aoifeDowney <aoifestard@gmail.com>
2020-01-27 21:56:42 +0000

    set up the transactions component

--- app/IndividualProfile/IndividualProfile.component.ts
+++ app/IndividualProfile/IndividualProfile.component.ts
@@ -2,27 +2,96 @@ import { Component, OnInit } from "@angular/core";
 import * as app from "application";
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 
+import { TransactionsService } from "../shared/transactions/transactions.service";
+
 @Component({
     selector: "gr-IndividualProfile",
     templateUrl: "./IndividualProfile.component.html",
-    styleUrls: ["./IndividualProfile.component.css"]
+    styleUrls: ["./IndividualProfile.component.css"],
+    providers: [TransactionsService]
 })
 export class IndividualProfileComponent implements OnInit {
 
-
     expensesChartData = [];
+    transactions = [];
+    textFieldValue = "";
+    rentTotal: number;
+    rentPercentage: number;
+    utilityBillsTotal: number;
+    utilityBillsPercentage: number;
+    houseShopTotal: number;
+    houseShopPercentage: number;
+    totalTransactions: number;
+    house: number[] = [];
 
-    constructor() {
-
+    constructor(private transactionsService: TransactionsService) {
         this.expensesChartData = [
-            { name: "Rent", ammount: 90 },
-            { name: "Utility Bills", ammount: 76 },
-            { name: "House Shop", ammount: 60 }
+            { name: "Rent", ammount: 69.5 },
+            { name: "Utility Bills", ammount: 19.60 },
+            { name: "House Shop", ammount: 10.80 }
         ]
     }
 
     ngOnInit(): void {
+        this.transactionsService.get().subscribe((data) => {
+            this.transactions = data;
+        }, () => {
+            alert({
+                title: "Transactions",
+                message: "An error occurred retrieving your data"
+            });
+        });
     }
+
+    save() {
+        var transaction = {
+          name: this.textFieldValue
+        };
+    
+        this.transactionsService.save(transaction).then((newTransaction) => {
+          this.transactions.unshift(newTransaction);
+        })
+        this.textFieldValue = "";
+      }
+
+      calculatePrice(type: string, price: number) {
+
+        const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+        if(type == "House Shop") {
+            this.house.push(price);
+            this.houseShopTotal = this.house.reduce(reducer);
+            this.houseShopTotal = this.houseShopTotal / 11;
+        }
+        else if(type == "Rent") {
+            this.house.push(price);
+            this.rentTotal = this.house.reduce(reducer);
+            this.rentTotal = this.rentTotal / 11;
+        }
+        else if(type == "Utility Bill") {
+            this.house.push(price);
+            this.utilityBillsTotal = this.house.reduce(reducer);
+            this.utilityBillsTotal = this.utilityBillsTotal / 11;
+        }
+        
+        this.totalTransactions = this.houseShopTotal;
+        this.totalTransactions = this.rentTotal
+        this.totalTransactions = this.utilityBillsTotal;
+
+        return this.totalTransactions;
+    }
+
+      getIcon(type: string) {
+          if(type == "House Shop") {
+              return "~/images/houseShop.png";
+          }
+          else if(type == "Rent") {
+              return "~/images/rent.png";
+          }
+          else if(type == "Utility Bill") {
+              return "~/images/utilityBill.png"
+          }
+      }
 
     onDrawerButtonTap(): void {
         const sideDrawer = <RadSideDrawer>app.getRootView();
---
+++ app/shared/transactions/transactions.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from "@angular/core";
+import * as Kinvey from "kinvey-nativescript-sdk";
+
+@Injectable()
+export class TransactionsService {
+
+    private dataStore;
+
+    constructor() {
+        this.dataStore = Kinvey.DataStore.collection("Transactions");
+    }
+
+    get() {
+        const query = new Kinvey.Query();
+        // Sort by descending “entity created time” to put new items on top.
+        query.descending("_kmd.ect");
+        return this.dataStore.find(query);
+    }
+
+    getHouseShopPrice() {
+        const query = new Kinvey.Query();
+        query.equalTo('type', 'HouseShop');
+        query.ascending('price');
+
+        return this.dataStore.find(query);
+    }
+
+    save(task) {
+        return this.dataStore.save(task);
+    }
+
+    handleErrors(error: Kinvey.Errors.BaseError) {
+        console.error(error.message);
+        return Promise.reject(error.message);
+    }
+}
